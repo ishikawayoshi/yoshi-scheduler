ea13c199d53eaefd20c1c1e1c592b5c7508cbec8
Aleksandr Shpak <shpak@netcracker.com>
2020-01-22 23:31:36 +0400

    initial commit

---
+++ src/main/java/com/ishikawayoshi/web/YoshiSchedulerWebStarterApplication.java
@@ -0,0 +1,13 @@
+package com.ishikawayoshi.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoshiSchedulerWebStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoshiSchedulerWebStarterApplication.class, args);
+    }
+
+}
